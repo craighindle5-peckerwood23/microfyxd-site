216fd10418c001383776061c085f826b2e077ea6
craighindle5-peckerwood23 <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-04-15 16:46:46 -0700

    Create tailwind.config.js

---
+++ app/tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      // Add custom colors here if needed
+    }
+  },
+  plugins: []
+};
