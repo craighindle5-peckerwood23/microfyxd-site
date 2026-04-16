fa22af4f6e4510fb4a319428051ce6fa6296fb25
craighindle5-peckerwood23 <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-04-16 04:29:59 -0700

    Refactor Home component to include multiple sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
-import Hero from "@/components/Hero";
+// app/page.tsx
+import Hero from './components/Hero'
+import HologramSection from './components/HologramSection'
+import NeuralLink from './components/NeuralLink'
+import GroqLPU from './components/GroqLPU'
+import Capabilities from './components/Capabilities'
+import OperatorConsoleSection from './components/OperatorConsoleSection'
+import MicroTaskEngine from './components/MicroTaskEngine'
+import FileAnalyzer from './components/FileAnalyzer'
+import WorkflowBuilder from './components/WorkflowBuilder'
+import PoweredByGroq from './components/PoweredByGroq'
+import FinalCTA from './components/FinalCTA'
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <main className="w-full overflow-x-hidden">
+      <Hero />
+      <HologramSection />
+      <NeuralLink />
+      <GroqLPU />
+      <Capabilities />
+      <OperatorConsoleSection />
+      <MicroTaskEngine />
+      <FileAnalyzer />
+      <WorkflowBuilder />
+      <PoweredByGroq />
+      <FinalCTA />
+    </main>
+  )
 }
