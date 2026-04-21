4652f0302021beac814d10550acf1cb1f18af763
craighindle5-peckerwood23 <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-04-20 20:58:51 -0700

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,11 @@
 import { redirect } from 'next/navigation'
 import { supabase } from '@/lib/supabaseClient'
 
-export default async function DashboardLayout({ children }) {
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   const { data } = await supabase.auth.getSession()
 
   if (!data.session) redirect('/auth/login')
