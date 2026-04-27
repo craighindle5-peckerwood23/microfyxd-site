c879ca1db6475561629eca52175f1e29bf7c61c7
craighindle5-peckerwood23 <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-04-27 10:50:54 -0700

    Create app/components/Hero.tsx

---
+++ app/app/components/Hero.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { HeroCanvas } from "./HeroCanvas";
+
+export default function Hero() {
+  return (
+    <section className="w-full flex items-center justify-center py-24">
+      <HeroCanvas />
+    </section>
+  );
+}
