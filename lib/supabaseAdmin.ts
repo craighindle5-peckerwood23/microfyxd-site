60c6777ac98336108a7ae215a68d2d03078383f0
craighindle5-peckerwood23 <247479351+craighindle5-peckerwood23@users.noreply.github.com>
2026-04-28 15:16:14 -0700

    fix: correct env var in supabaseAdmin

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,12 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
   {
     auth: {
       autoRefreshToken: false,
-      persistSession: false
-    }
+      persistSession: false,
+    },
   }
 );
